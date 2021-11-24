cb8fd8a1aa36623c7187e2a1b07c10d5e5b4f845
bearddan2000 <bearddan2000@yahoo.com>
2021-11-24 10:49:25 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# python-cli-pydoc-pascal-functional-prog
+
+## Description
+Find the binomial coefficent.
+Follows functional programming practices
+POC for pydoc.
+
+## Tech stack
+- python 3.8
+  - pydoc
+
+## Docker stack
+- python:latest
+
+## To run
+`sudo ./install.sh -u`
+
+## To stop (optional)
+`sudo ./install.sh -d`
+
+## For help
+`sudo ./install.sh -h`
+
+## Credits
+https://www.onlinegdb.com/online_python_compiler
---
+++ bin/main.py
@@ -0,0 +1,34 @@
+"""Find the binomial coefficent."""
+def row(k, i, c):
+    """Prints row of coefficents
+    :param k: start pos in row
+    :param i: iteration index
+    :param c: running coefficent
+    :return:
+    """
+    if k > i:
+        return;
+    print( "%d, " % c, end="");
+    a = c * (i-k)/(k+1);
+    return row(k+1, i, a);
+
+def col(i, n):
+    """Prints row of coefficents
+    then a newline character.
+    :param i: coefficent limit
+    :param n: colummn number
+    :return:
+    """
+    if i > n:
+        return;
+    row(0, i, 1);
+    print("");
+    return col(i+1, n);
+
+def main():
+    i = 10;
+    print( "[INPUT] %d" % i);
+    print( "[OUTPUT] ");
+    col(0, i);
+
+main()
